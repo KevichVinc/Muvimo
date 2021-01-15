bf20268a85b6a0c309dfad33d6ec79c6232f0d3b
KevichVinc <kevich.vinc@gmail.com>
2021-01-15 12:37:45 +0300

    navbar & react-router-dom

--- frontendPart/src/index.jsx
+++ frontendPart/src/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
 
-const title = 'React with Webpack and Babel and we doomed';
-
 ReactDOM.render(
-  <App title={title} />,
+  <Router>
+    <App />
+  </Router>,
   document.getElementById('app'),
 );
 
