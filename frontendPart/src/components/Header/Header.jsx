ec8726d47c0a039afef7f30a24145833a0bf8da6
KevichVinc <kevich.vinc@gmail.com>
2021-01-16 18:15:19 +0300

    friends grid, logo

--- frontendPart/src/components/Header/Header.jsx
+++ frontendPart/src/components/Header/Header.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 import style from './Header.module.css';
+import muvimo from '../../assets/img/Muvimo-logo.png';
 
 export default function Header() {
   return (
     <div className={style.header}>
-      {' '}
-      <NavLink to="/feed">Header</NavLink>
+      <NavLink to="/feed">
+        <img src={muvimo} alt="logo" />
+      </NavLink>
     </div>
   );
 }
--- frontendPart/src/components/Profile/Friendsbox/Friendsbox.jsx
+++ frontendPart/src/components/Profile/Friendsbox/Friendsbox.jsx
@@ -6,7 +6,36 @@ export default function Friendsbox() {
     <div className={style.main}>
       <div className={style.friends}>All Friends</div>
       <div className={style.counter}>365</div>
-      <div className={style.friendlist}>img</div>
+      <img
+        className={style.friendlist}
+        src="https://icon-library.com/images/icon-avatar/icon-avatar-16.jpg"
+        alt="ava"
+      />
+      <img
+        className={style.friendlist}
+        src="https://icon-library.com/images/icon-avatar/icon-avatar-16.jpg"
+        alt="ava"
+      />
+      <img
+        className={style.friendlist}
+        src="https://icon-library.com/images/icon-avatar/icon-avatar-16.jpg"
+        alt="ava"
+      />
+      <img
+        className={style.friendlist}
+        src="https://icon-library.com/images/icon-avatar/icon-avatar-16.jpg"
+        alt="ava"
+      />
+      <img
+        className={style.friendlist}
+        src="https://icon-library.com/images/icon-avatar/icon-avatar-16.jpg"
+        alt="ava"
+      />
+      <img
+        className={style.friendlist}
+        src="https://icon-library.com/images/icon-avatar/icon-avatar-16.jpg"
+        alt="ava"
+      />
     </div>
   );
 }
