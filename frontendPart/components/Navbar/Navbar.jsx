3da98ec7e3c9c0a647fce057b8f90e5dd84ec2d5
RedEscape <wachers511@gmail.com>
2021-02-10 10:21:45 +0300

    fix-style-navbar

--- frontendPart/components/Navbar/Navbar.jsx
+++ frontendPart/components/Navbar/Navbar.jsx
@@ -10,99 +10,118 @@ import style from './Navbar.module.css';
 
 export default function Navbar() {
   return (
-    <nav className={style.nav}>
-      <div className={style.item}>
-        <NavLink to="/profiles" activeClassName={style.active}>
-          <img
-            className={style.iconMenu}
-            src={avatarPNG}
-            alt="profiles"
-          />
-        </NavLink>
-        <NavLink
-          to="/profiles"
-          className={style.textMenu}
-          activeClassName={style.active}
-        >
-          Profiles
-        </NavLink>
-      </div>
-      <div className={style.item}>
-        <NavLink to="/settings" activeClassName={style.active}>
-          <img
-            className={style.iconMenu}
-            src={settingsPNG}
-            alt="settings"
-          />
-        </NavLink>
-        <NavLink
-          to="/settings"
-          className={style.textMenu}
-          activeClassName={style.active}
-        >
-          Settings
-        </NavLink>
-      </div>
-      <div className={style.item}>
-        <NavLink to="/portfolio" activeClassName={style.active}>
-          <img
-            className={style.iconMenu}
-            src={folderPNG}
-            alt="portfolio"
-          />
-        </NavLink>
-        <NavLink
-          to="/portfolio"
-          className={style.textMenu}
-          activeClassName={style.active}
-        >
-          Portfolio
-        </NavLink>
-      </div>
-      <div className={style.item}>
-        <NavLink to="/feed" activeClassName={style.active}>
-          <img className={style.iconMenu} src={tagPNG} alt="feed" />
-        </NavLink>
-        <NavLink
-          to="/feed"
-          className={style.textMenu}
-          activeClassName={style.active}
-        >
-          Feed
-        </NavLink>
-      </div>
-      <div className={style.item}>
-        <NavLink to="/newprofile" activeClassName={style.active}>
-          <img
-            className={style.iconMenu}
-            src={editPNG}
-            alt="newprofile"
-          />
-        </NavLink>
-        <NavLink
-          to="/newprofile"
-          className={style.textMenu}
-          activeClassName={style.active}
-        >
-          Create Profile
-        </NavLink>
-      </div>
-      <div className={style.item}>
-        <NavLink to="/find?firstName=" activeClassName={style.active}>
-          <img
-            className={style.iconMenu}
-            src={searchPNG}
-            alt="global search"
-          />
-        </NavLink>
-        <NavLink
-          to="/find?firstName="
-          className={style.textMenu}
-          activeClassName={style.active}
-        >
-          Global Search
-        </NavLink>
-      </div>
-    </nav>
+    <div className={style.main}>
+      <nav className={style.nav}>
+        <div className={style.item}>
+          <NavLink
+            to="/profiles"
+            className={style.textMenu}
+            activeClassName={style.active}
+          >
+            Profiles
+          </NavLink>
+        </div>
+        <div className={style.item}>
+          <NavLink
+            to="/settings"
+            className={style.textMenu}
+            activeClassName={style.active}
+          >
+            Settings
+          </NavLink>
+        </div>
+        <div className={style.item}>
+          <NavLink
+            to="/portfolio"
+            className={style.textMenu}
+            activeClassName={style.active}
+          >
+            Portfolio
+          </NavLink>
+        </div>
+        <div className={style.item}>
+          <NavLink
+            to="/feed"
+            className={style.textMenu}
+            activeClassName={style.active}
+          >
+            Feed
+          </NavLink>
+        </div>
+        <div className={style.item}>
+          <NavLink
+            to="/newprofile"
+            className={style.textMenu}
+            activeClassName={style.active}
+          >
+            Create Profile
+          </NavLink>
+        </div>
+        <div className={style.item}>
+          <NavLink
+            to="/find?firstName="
+            className={style.textMenu}
+            activeClassName={style.active}
+          >
+            Global Search
+          </NavLink>
+        </div>
+      </nav>
+      <nav className={style.navMini}>
+        <div className={style.item}>
+          <NavLink to="/profiles" activeClassName={style.active}>
+            <img
+              className={style.iconMenu}
+              src={avatarPNG}
+              alt="profiles"
+            />
+          </NavLink>
+        </div>
+        <div className={style.item}>
+          <NavLink to="/settings" activeClassName={style.active}>
+            <img
+              className={style.iconMenu}
+              src={settingsPNG}
+              alt="settings"
+            />
+          </NavLink>
+        </div>
+        <div className={style.item}>
+          <NavLink to="/portfolio" activeClassName={style.active}>
+            <img
+              className={style.iconMenu}
+              src={folderPNG}
+              alt="portfolio"
+            />
+          </NavLink>
+        </div>
+        <div className={style.item}>
+          <NavLink to="/feed" activeClassName={style.active}>
+            <img className={style.iconMenu} src={tagPNG} alt="feed" />
+          </NavLink>
+        </div>
+        <div className={style.item}>
+          <NavLink to="/newprofile" activeClassName={style.active}>
+            <img
+              className={style.iconMenu}
+              src={editPNG}
+              alt="newprofile"
+            />
+          </NavLink>
+        </div>
+        <div className={style.item}>
+          <NavLink
+            to="/find?firstName="
+            activeClassName={style.active}
+          >
+            <img
+              className={style.iconMenu}
+              src={searchPNG}
+              alt="global search"
+            />
+          </NavLink>
+        </div>
+      </nav>
+    </div>
   );
 }
