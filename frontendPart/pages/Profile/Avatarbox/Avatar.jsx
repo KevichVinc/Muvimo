b90dbf60f13c2f882c767a19d3cc5faac04a187b
KevichVinc <kevich.vinc@gmail.com>
2021-02-10 17:18:13 +0300

    feature-profile-style

--- frontendPart/pages/Profile/Avatarbox/Avatar.jsx
+++ frontendPart/pages/Profile/Avatarbox/Avatar.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { getProfileAvatar } from '../../../redux/selectors/profiles';
-import style from './Avatar.module.css';
+import { NavLink } from 'react-router-dom';
+import * as profileSelectors from '../../../redux/selectors/profiles';
+import style from '../Profile.module.css';
 
 export default function Avatar() {
-  const avatar = useSelector(getProfileAvatar);
+  const avatar = useSelector(profileSelectors.getProfileAvatar);
+  const firstName = useSelector(profileSelectors.getProfileFirstName);
+  const lastName = useSelector(profileSelectors.getProfileLastName);
   return (
-    <div className={style.main}>
-      <div className={style.ava}>
-        <img src={avatar} alt="ava" />
+    <div className={style.userShortDescription}>
+      <NavLink to="#!" className={style.avatarLink}>
+        <div className={style.avatarBorder} />
+        <img
+          className={style.avatarContent}
+          src={avatar}
+          alt="avatar"
+        />
+      </NavLink>
+      <div className={style.shortDescriptionFullName}>
+        {`${firstName} ${lastName}`}
       </div>
     </div>
   );
